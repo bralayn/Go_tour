9ada9f385c27ffc02c2d2d8bc75c61eeff113915
Anatoli <kamotskys@gmail.com>
2020-12-09 20:36:42 +0300

    initializers variables, work in home

---
+++ variables-with-initializers.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var i, j, k int = 1, 2, 66
+
+func main() {
+	var c, pyton, java = true, false, "no!!!"
+	fmt.Println(i, j, c, pyton, java)
+	fmt.Println("k=", k)
+}
