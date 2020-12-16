ceec6b7da019e16cca21173b99b9f0a1b1d4665b
Anatoli <kamotskys@gmail.com>
2020-12-16 20:37:56 +0300

    My commit type-inference

---
+++ type-inference.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42  //change me !
+	fmt.Printf("v is of type %T\n", v)
+
+	var vv = 42.6  //change me !
+	fmt.Printf("vv is of type %T\n", vv)
+}
