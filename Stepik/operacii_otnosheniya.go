bd6532db838e0cac430d20e55c0aa56bfabc5655
Anatoli <kamotskys@gmail.com>
2021-02-10 22:09:16 +0300

    По данному трехзначному числу определите, все ли его цифры различны.
    Формат входных данных
    На вход подается одно натуральное трехзначное число.
    
    Формат выходных данных
    Выведите "YES", если все цифры числа различны, в противном случае - "NO".
    
    Sample Input:
    
    237
    Sample Output:
    
    YES

--- Stepik/operacii_otnosheniya.go
+++ Stepik/operacii_otnosheniya.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	x := n % 10
+	y := n % 100 / 10
+	z := n % 1000 / 100
+
+	switch {
+	case x != y && x != z && y != z:
+		fmt.Println("YES")
+	default:
+		fmt.Println("NO")
+	}
+
+	//fmt.Println(x, y, z)
 }
