3a4676a48c723b8103dde0403cd2a9ccc3b4beef
Anatoli <kamotskys@gmail.com>
2021-02-13 14:56:16 +0300

    Напишите программу, которая выводит квадраты натуральных чисел от 1 до 10. Квадрат каждого числа должен выводится в новой строке.

---
+++ Stepik/cicli.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//sum := 0
+	//for i := 1; i < 10; i++ {
+	//	sum += i
+	//}
+	//fmt.Println(sum)
+	//
+
+	var i = 1
+	for ; i <= 10; i++ {
+		fmt.Println("i=", i, i*i)
+	}
+
+}
